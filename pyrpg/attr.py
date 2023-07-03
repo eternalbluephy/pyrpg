016bf5331b258aea5646f26ac923144fff54ed8b
eternalbluephy <peihongyu123@126.com>
2023-07-03 20:18:18 +0800

    modified pyrpg/entity.pu

--- pyrpg/attr.py
+++ pyrpg/attr.py
@@ -16,3 +16,31 @@ class Attr:
 
     def set_name(self, name: str):
         self.name = name
+
+    def set_value(self, value: Any):
+        self.value = value
+
+    def vary_to(self, amount: float):
+        self._check_variable()
+        if (self.upper is not None and amount > self.upper):
+            amount = self.upper
+        elif (self.lower is not None and amount < self.lower):
+            amount = self.lower
+        self.set_value(amount)
+
+    def vary_by_amount(self, amount: float):
+        self._check_variable()
+        ret = self.value + amount
+        self.set_value(ret)
+
+    def vary_by_percent(self, percent: float):
+        self._check_variable()
+        ret = self.value * (1 + percent)
+        self.set_value(ret)
+
+    def is_variable(self):
+        return isinstance(self.value, int) or isinstance(self.value, float)
+
+    def _check_variable(self):
+        if (not self.is_variable()):
+            raise TypeError(f"Attribute '{self.name}' is not variable.")
--- pyrpg/entity.py
+++ pyrpg/entity.py
@@ -1,13 +1,13 @@
 from abc import ABC, abstractmethod
 from json import load
 from typing import Any
+from attr import Attr
 
 class Entity(ABC):
     '''
     Entity is a basic model for every mob, character, enemy or customized creature. It doesn't contains any specific data, but some methods to load and  get data.
     '''
-
-    def get_data(self, name: str) -> Any:
+    def get_data(self, name: str) -> Attr | None:
         '''
         Get the entity's data according name.
         Args:
